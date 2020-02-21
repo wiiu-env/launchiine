c71d82020228db87ec7597add724adfb62132ad5
Maschell <Maschell@gmx.de>
2020-02-21 14:24:17 +0100

    Increase the maximum async element list and change the time to wait

--- src/utils/AsyncExecutor.cpp
+++ src/utils/AsyncExecutor.cpp
@@ -21,7 +21,7 @@ AsyncExecutor::AsyncExecutor() {
                 }
             }
             instance->mutex.unlock();
-            std::this_thread::sleep_for(std::chrono::milliseconds(50));
+            std::this_thread::sleep_for(std::chrono::milliseconds(16));
             DCFlushRange((void*)&exitThread, sizeof(exitThread));
         }
     });
@@ -34,8 +34,8 @@ void AsyncExecutor::execute(std::function<void()> func) {
         instance = new AsyncExecutor();
     }
 
-    while(instance->elements.size() > 10) {
-        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+    while(instance->elements.size() > 25) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(16));
     }
 
     instance->mutex.lock();
