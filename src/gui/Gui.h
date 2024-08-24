f6f33b52f12dbf7e096c93857ad0a9b001d4ec97
Blackjay07 <116186005+rdistiso@users.noreply.github.com>
2024-08-24 00:09:35 +0000

    Add Gui.h

---
+++ src/gui/Gui.h
@@ -0,0 +1,40 @@
+/****************************************************************************
+ * Copyright (C) 2015 Dimok
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ****************************************************************************/
+#ifndef __GUI_H
+#define __GUI_H
+
+#include <gui/FreeTypeGX.h>
+#include <gui/GameBgImage.h>
+#include <gui/GridBackground.h>
+#include <gui/GuiButton.h>
+#include <gui/GuiCheckBox.h>
+#include <gui/GuiController.h>
+#include <gui/GuiDragListener.h>
+#include <gui/GuiElement.h>
+#include <gui/GuiFrame.h>
+#include <gui/GuiImage.h>
+#include <gui/GuiImageAsync.h>
+#include <gui/GuiImageData.h>
+#include <gui/GuiParticleImage.h>
+#include <gui/GuiScrollbar.h>
+#include <gui/GuiSelectBox.h>
+#include <gui/GuiSound.h>
+#include <gui/GuiSwitch.h>
+#include <gui/GuiText.h>
+#include <gui/GuiToggle.h>
+#include <gui/GuiTrigger.h>
+#include <gui/VPadController.h>
+#include <gui/WPadController.h>
+
+#endif
